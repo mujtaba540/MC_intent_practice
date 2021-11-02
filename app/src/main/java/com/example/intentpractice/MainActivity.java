e3024c659f093153f8b9ddcb1c8071d1c701cc4e
mujtaba <mujtaba@salesassist.pk>
2021-11-02 20:48:29 +0500

    next activity listner added

--- app/src/main/java/com/example/intentpractice/MainActivity.java
+++ app/src/main/java/com/example/intentpractice/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.intentpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button next,url,phone,data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        next=findViewById(R.id.next_page);
+        url=findViewById(R.id.url);
+        phone=findViewById(R.id.dial);
+        data=findViewById(R.id.data);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,MainActivity3.class);
+                startActivity(intent);
+            }
+        });
     }
 }
